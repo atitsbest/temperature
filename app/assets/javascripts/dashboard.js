fa9b97b999f6fc8c3843fdf35c6dfd01d036a8bc
meist <stephan.meissner@dccs.at>
2013-11-07 16:37:28 +0100

    + Sensordaten werden in den Charts verwendet.

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,13 +1,7 @@
 //= require d3.v3
 
 (function() {
-  var parse = d3.time.format("%b %Y").parse;
-  // Parse dates and numbers. We assume values are sorted by date.
-  function type(d) {
-    d.date = parse(d.date);
-    d.price = +d.price;
-    return d;
-  }
+  var parse = d3.time.format.utc("%Y-%m-%dT%H:%M:%S.%LZ").parse;
 
   /**
    * Chart erstellen und platzieren.
@@ -15,6 +9,10 @@
    * @param element Hier soll der Chart erstellt werden.
    */
   function placeChart(element, data) {
+    var sensor = $(element).data('sensor');
+
+    if (sensor === undefined) return;
+
     var margin = {top: 0, right: 0, bottom: 20, left: 0},
         width = parseInt(d3.select(element).style('width'), 10) - margin.left - margin.right,
         height = 100 - margin.top - margin.bottom;
@@ -28,25 +26,25 @@
     // An area generator, for the light fill.
     var area = d3.svg.area()
         .interpolate("monotone")
-        .x(function(d) { return x(d.date); })
+        .x(function(d) { return x(d.created_at); })
         .y0(height)
-        .y1(function(d) { return y(d.price); });
+        .y1(function(d) { return y(d.value); });
 
     // A line generator, for the dark stroke.
     var line = d3.svg.line()
         .interpolate("monotone")
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.price); });
+        .x(function(d) { return x(d.created_at); })
+        .y(function(d) { return y(d.value); });
 
 
-    // Filter to one symbol; the S&P 500.
+    // Filter to one sensor;
     var values = data.filter(function(d) {
-      return d.symbol == "S&P 500";
+      return d.sensor == sensor;
     });
 
-    // Compute the minimum and maximum date, and the maximum price.
-    x.domain([values[0].date, values[values.length - 1].date]);
-    y.domain([0, d3.max(values, function(d) { return d.price; })]).nice();
+    // Compute the minimum and maximum date, and the maximum value.
+    x.domain([values[0].created_at, values[values.length - 1].created_at]);
+    y.domain([0, d3.max(values, function(d) { return d.value; })]).nice();
 
     // Add an SVG element with the desired dimensions and margin.
     var svg = d3.select(element).append("svg")
@@ -87,19 +85,19 @@
         .attr("clip-path", "url(#clip)")
         .attr("d", line(values));
 
-    // Add a small label for the symbol name.
+    // Add a small label for the sensor name.
     svg.append("text")
         .attr("x", width - 6)
         .attr("y", height - 6)
         .style("text-anchor", "end")
-        .text(values[0].symbol);
+        .text(values[0].sensor);
 
     // On click, update the x-axis.
     function click() {
       var n = values.length - 1,
           i = Math.floor(Math.random() * n / 2),
           j = i + Math.floor(Math.random() * n / 2) + 1;
-      x.domain([values[i].date, values[j].date]);
+      x.domain([values[i].created_at, values[j].created_at]);
       var t = svg.transition().duration(750);
       t.select(".x.axis").call(xAxis);
       t.select(".area").attr("d", area(values));
@@ -108,12 +106,17 @@
   }
 
   $(function() {
-    d3.csv("readme.csv", type, function(error, data) {
+    d3.json("/api/measurements.json", function(error, data) {
       if (!error) {
+        $(data).each(function(i, d) {
+          d.created_at = parse(d.created_at);
+          d.value = +d.value;
+        });
         $(".panel-body").each(function(i, panel) {
           placeChart(panel, data);
         });
       }
+      else alert(error);
     });
   });
 
